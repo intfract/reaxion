4462edfefc28a980c24cb2823bbea3e37156765b
Int Fract <110207136+intfract@users.noreply.github.com>
2023-04-21 09:55:29 +0000

    changed stuff

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 import fs from 'fs'
+
+function compile(script) {
+  
+}
+
+function compileFile(filePath, outputPath) {
+  const file = fs.readFileSync(filePath, 'utf-8')
+  fs.writeFileSync(outputPath, compile(file))
+}
